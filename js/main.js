742e7a78a55a23bb43936dce22e7402be2b7401e
standvic <standvic@gmail.com>
2018-12-26 23:34:29 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,21 +1,24 @@
 // main module (classes)
 
-function App(hub, engine) {
+function App() {
+    this.hub = new Hub();
     this.init = function () {
-        hub.create();
-        engine.start();
+        this.hub.create();
+        //engine.start();
     };
 };
 
 function Hub(params) {
     this.border = true;
     this.width = params.width || '500px';
+    console.log(this.width);
     this.height = params.height || '500px';
     this.backgroundColor = params.backgroundColor || 'white';
     this.color = params.color || 'black';
 
     this.create = function()  {
         var bodyElem = window.body;
+        console.log(bodyElem);
     };
 
 };
